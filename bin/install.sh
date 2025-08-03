ea14d3525f85e0a5d76ebd8cc0ae55553a0f5927
Thế Thắng Trần <tranthethang@gmail.com>
2025-08-03 17:05:56 +0700

    upgrade node packages
    Former-commit-id: 4de5039fc1300ac277ad3d4d1e37ddf663de1d00

--- bin/install.sh
+++ bin/install.sh
@@ -4,9 +4,7 @@
 # docker exec -it $(docker ps -a | grep origamiez | cut -c1-4) bash
 
 # password: secret
-sudo chmod 777 -R ./docker &&
 docker exec -it $(docker ps -a | grep origamiez | cut -c1-4) apt update &&
 docker exec -it $(docker ps -a | grep origamiez | cut -c1-4) apt install -y default-mysql-client &&
 docker exec -it $(docker ps -a | grep origamiez | cut -c1-4) /tmp/snapshot/restore.sh &&
-docker exec -it $(docker ps -a | grep origamiez | cut -c1-4) /tmp/snapshot/composer.sh &&
-docker exec -it $(docker ps -a | grep origamiez | cut -c1-4) /tmp/snapshot/restore-media.sh
+docker exec -it $(docker ps -a | grep origamiez | cut -c1-4) /tmp/snapshot/composer.sh
