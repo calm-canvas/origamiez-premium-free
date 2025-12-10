78672136b8f371720cc0ff193d6633d3c62e8c59
Thế Thắng Trần <tranthethang@gmail.com>
2025-12-10 10:24:36 +0700

    improve factory sample data

--- bin/init.sh
+++ bin/init.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-docker compose exec --user root wordpress chmod 755 -R  /var/www/html/wp-content &&
+docker compose exec --user root cli chmod 755 -R  /var/www/html/wp-content &&
 docker compose exec --user root cli chown -R www-data:www-data /var/www/html/wp-content &&
+docker compose exec --user root wordpress chmod 755 -R  /var/www/html/wp-content &&
+docker compose exec --user root wordpress chown -R www-data:www-data /var/www/html/wp-content &&
 docker compose exec cli wp plugin install theme-check &&
 docker compose exec cli wp plugin install wordpress-importer
 
