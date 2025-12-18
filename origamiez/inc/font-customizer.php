af9d83d2ab04c6f976ebd2e60699ad88a6ac3932
Thế Thắng Trần <tranthethang@gmail.com>
2025-12-18 21:07:56 +0700

    refactor: consolidate and organize helper functions into separate files

---
+++ origamiez/inc/font-customizer.php
@@ -0,0 +1,28 @@
+<?php
+
+use Origamiez\Engine\Config\FontCustomizerConfig;
+
+function origamiez_get_font_families()
+{
+    return FontCustomizerConfig::get_font_families();
+}
+
+function origamiez_get_font_sizes()
+{
+    return FontCustomizerConfig::get_font_sizes();
+}
+
+function origamiez_get_font_styles()
+{
+    return FontCustomizerConfig::get_font_styles();
+}
+
+function origamiez_get_font_weights()
+{
+    return FontCustomizerConfig::get_font_weights();
+}
+
+function origamiez_get_font_line_heighs()
+{
+    return FontCustomizerConfig::get_font_line_heights();
+}
