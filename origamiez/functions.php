5fc3144640fcc1d97a804d4f51718606217b9e20
Thế Thắng Trần <tranthethang@gmail.com>
2025-12-18 18:02:59 +0700

    refactor inc func

--- origamiez/functions.php
+++ origamiez/functions.php
@@ -1,4 +1,7 @@
 <?php
+
+use Origamiez\Engine\ThemeBootstrap;
+
 const ORIGAMIEZ_PREFIX = 'origamiez_';
 $dir                   = trailingslashit( get_template_directory() );
 
@@ -7,7 +10,7 @@ require_once $dir . '/vendor/autoload.php';
 add_action(
 	'after_setup_theme',
 	function () {
-		$bootstrap = new \Origamiez\Engine\ThemeBootstrap();
+		$bootstrap = new ThemeBootstrap();
 		$bootstrap->boot();
 	}
 );
@@ -26,10 +29,6 @@ PLUGINS
 Setup - config for compatible plugins.
 --------------------
 */
-// 1: bbPress
 require $dir . 'plugins/bbpress/index.php';
-// 2: DW Question & Answer
 require $dir . 'plugins/dw-question-and-answer/index.php';
-// 3: WooCommerce
 require $dir . 'plugins/woocommerce/index.php';
-
