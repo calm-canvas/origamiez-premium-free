247ca9cb69cd897a064518cdd26c10c7590f0e3d
Thế Thắng Trần <tranthethang@gmail.com>
2025-12-31 14:53:41 +0700

    add craftsman-suite

--- bin/backupdb.sh
+++ bin/backupdb.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-TIMESTAMP=$(date +%Y%m%d_%H%M%S)
-docker compose exec cli wp db export /tmp/snapshot/backup_${TIMESTAMP}.sql
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)  &&
+docker compose exec --user root cli chmod 755 -R  /tmp/snapshot &&
+docker compose exec --user root cli chown -R www-data:www-data /tmp/snapshot &&
+docker compose exec cli wp db export /tmp/snapshot/backup_${TIMESTAMP}.sql  &&
 docker compose exec cli gzip /tmp/snapshot/backup_${TIMESTAMP}.sql
