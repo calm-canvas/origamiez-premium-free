72e4bb13870a31ebb82f743a4e41b6712729a668
Thế Thắng Trần <tranthethang@gmail.com>
2026-03-16 22:27:53 +0700

    chore: update version to 4.0.1 - update version number in style.css and vite.config.js - remove unused editor-style.css

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve, dirname } from 'path';
 import fs from 'fs';
 
-const STYLE_VERSION = `4.0.0`;
+const STYLE_VERSION = `4.0.1`;
 
 const stringReplacePlugin = () => {
 	return {
